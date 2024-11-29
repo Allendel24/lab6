bf2df00452dc1a09e59d0c8ba9b3e55e83500a95
Film_ <pawich23013@gmail.com_>
2024-11-29 16:38:11 +0700

    Filled out lab6_1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x = 1;
+    int y;
+    int n = 0;
+    int m = 0;
+    while(x != 0){
+        cout << "Enter an integer: ";
+        cin >> y;
+        x = y;
+        if(y%2==0 and y != 0){
+            n++;
+        } else{m++;}
+    }
+    cout << "#Even numbers = " << n << endl;
+    cout << "#Odd numbers = " << m-1;
     return 0;
 }
